c0f73bd3026fe79d7dd7c5767192ac7956146e21
VincentOL <vincentohwell@gmail.com>
2017-11-02 16:03:30 -0700

    database setup and routed

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -2,10 +2,12 @@ const functions = require('firebase-functions');
 const express = require('express');
 
 const app = express();
+const dbroutes = require('./routes/database.js');
 
 app.get('/testroute', (request, response) => {
-  console.log("HERE");
   response.send({"DummyData": "Truly Dummy Data"});
 });
+
+app.get('/dbtest', dbroutes.test);
 
 exports.app = functions.https.onRequest(app);
