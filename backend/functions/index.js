347ca9337b50a1de032d51a53ac764d107149e4e
Daniel Kostinskiy <Danielkostinskiy@gmail.com>
2017-11-05 22:23:44 -0800

    read and write functions to databases.

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,9 +1,53 @@
-const functions = require('firebase-functions');
-const express = require('express');
 
+var functions = require('firebase-functions');
+const express = require('express');
+var url = require('url');
+var admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 const app = express();
 const dbroutes = require('./routes/database.js');
+var db = admin.database();
 
+
+
+exports.writeUser = functions.https.onRequest((req, res) => {
+	
+		res.header('Access-Control-Allow-Origin', '*');
+ 	 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');	
+		var query = url.parse(req.url, true).query;
+		var user = "";
+		var name = "";
+		var password = "";
+		if(query['username']){
+			user = query['username'];
+		}
+		if(query['name']){
+			name = query['name'] ;
+		}
+		if(query['password']){
+			password = query['password'];
+		}
+		var usersRef = db.ref("users");	
+		//  writes to database.
+		usersRef.child(name).set({
+  			username: user,
+			password: password	
+ 		});
+		res.send("Name of person: "+ name + " ,Username of person: "+ user+ " ,Password of person:" + password);
+});
+exports.readUser = functions.https.onRequest((req, res) => {
+	
+		res.header('Access-Control-Allow-Origin', '*');
+ 	 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');	
+		var usersRef = admin.database().ref().child('users');
+		usersRef.on('value', snap=> {
+			res.send(snap.val());
+		});
+		
+		
+});
 app.get('/testroute', (request, response) => {
   response.send({"DummyData": "Truly Dummy Data"});
 });
