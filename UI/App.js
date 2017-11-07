993c322c2086763ac2c6734cabf2f2120612aad1
VincentOL <vincentohwell@gmail.com>
2017-11-07 00:13:20 -0800

    typo

--- UI/App.js
+++ UI/App.js
@@ -37,7 +37,7 @@ export default class App extends Component<{}> {
           >
          <View style={{marginTop: 22}}>
           <View>
-            <Text>Settingsc</Text>
+            <Text>Setting</Text>
             <TouchableHighlight onPress={() => {
               this.setModalVisible(!this.state.modalVisible)
             }}>
